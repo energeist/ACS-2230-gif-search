79b79fb1c367dad522af591b55b8dfaa4d7d980c
Mark Rattle <mark.rattle@gmail.com>
2023-03-29 18:25:40 -0400

    added styling

--- app.js
+++ app.js
@@ -6,24 +6,25 @@ const express = require('express');
 
 // Require tenorjs near the top of the file
 const Tenor = require("tenorjs").client({
-    // Replace with your own key
-    "Key": process.env.TENOR_API_KEY, // https://tenor.com/developer/keyregistration
-    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
-    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
-  });
+  // Replace with your own key
+  "Key": process.env.TENOR_API_KEY, // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
 
 // App Setup
 const app = express();
+app.use(express.static('public'));
 
 // Middleware
 //allow Express (our web framework) to render HTML templates and send them back to the client using a new function
 
 const hbs = handlebars.create({
-    // Specify helpers which are only registered on this instance.
-    helpers: {
-        foo() { return 'FOO!'; },
-        bar() { return 'BAR!'; }
-    }
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    foo() { return 'FOO!'; },
+    bar() { return 'BAR!'; }
+  }
 });
 
 app.engine('handlebars', hbs.engine);
@@ -32,31 +33,31 @@ app.set('views', './views');
 
 // Routes
 app.get('/', (req, res) => {
-    // Handle the home page when we haven't queried yet
-    term = ""
-    if (req.query.term) {
-        term = req.query.term
-    }
-    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
-    Tenor.Search.Query(term, "10")
-        .then(response => {
-            // store the gifs we get back from the search
-            const gifs = response;
-            // pass the gifs as an object into the home page
-            res.render('home', { gifs })
-        }).catch(console.error);
-  })
+  // Handle the home page when we haven't queried yet
+  term = ""
+  if (req.query.term) {
+    term = req.query.term
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+    .then(response => {
+      // store the gifs we get back from the search
+      const gifs = response;
+      // pass the gifs as an object into the home page
+      res.render('home', { gifs })
+    }).catch(console.error);
+})
 
 app.get('/greetings/:name', (req, res) => {
-    // grab the name from the path provided
-    const name = req.params.name;
-    // render the greetings view, passing along the name
-    res.render('greetings', { name });
-  })
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
+})
 
 // Start Server
 
 //tells the app to log a message on port 3000. Once we start the server, we should see that message in the terminal 
 app.listen(3000, () => {
-    console.log('Gif Search listening on port localhost:3000!');
+  console.log('Gif Search listening on port localhost:3000!');
 });
